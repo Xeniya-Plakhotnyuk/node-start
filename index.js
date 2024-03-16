ce69371a054cba642f218e156e7eba9252e3a6da
XeniyaZ <kseniya10081989@gmail.com>
2024-03-16 18:57:32 -0500

    add post test

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-const http = require('http')
+// const http = require('http')
 
-const server = http.createServer((req, res) =>{
-if(req.url==='/'){
-  res.write("Main page")
-  res.end()
-}
-else if (req.url==='/about'){
-res.write("About us!")
-res.end()
-}
-else if(req.url==='/services'){
-  res.write("Our services")
-  res.end()
-}
-else{
-  res.write("Ooops! Page not found!")
-  res.end()
-}
+// const server = http.createServer((req, res) =>{
+// if(req.url==='/'){
+//   res.write("Main page")
+//   res.end()
+// }
+// else if (req.url==='/about'){
+// res.write("About us!")
+// res.end()
+// }
+// else if(req.url==='/services'){
+//   res.write("Our services")
+//   res.end()
+// }
+// else{
+//   res.write("Ooops! Page not found!")
+//   res.end()
+// }
 
-})
+// })
 
-server.listen(3000)
-console.log("my server is working on port 3000")
+// server.listen(3000)
+// console.log("my server is working on port 3000")
--- server.js
+++ server.js
@@ -1,13 +1,41 @@
+// const express = require('express')
+// const app = express()
+// const recipes = require("./recipes.js")
+// console.log(recipes)
+
+// app.get('/api/recipes', (req,res) =>{
+//     res.json(recipes)
+// })
+
+
+// app.listen(3000, () =>{
+// console.log("Port is on 3000...")
+// })
+
+
+
+
+
 const express = require('express')
 const app = express()
-const recipes = require("./recipes.js")
-console.log(recipes)
+const items = require('./data.js')
 
-app.get('/api/recipes', (req,res) =>{
-    res.json(recipes)
+app.get('/items', (req, res) =>{
+    res.json(items)
+})
+
+app.post("/items", (req, res) => {
+  const newItem = {
+    name: req.body.name,
+    id: req.body.id,
+    price: req.body.price,
+  }
+  items.push()
 })
 
 
-app.listen(3000, () =>{
-console.log("Port is on 3000...")
-})
+
+app.listen(4000, () =>{
+    console.log('My port is open at 4000...')
+}
+)
