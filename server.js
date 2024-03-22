f801d8b122d9d2b09f1cc4f0642b19bdc9caf23d
XeniyaZ <kseniya10081989@gmail.com>
2024-03-22 09:25:36 -0500

    add router

--- server.js
+++ server.js
@@ -1,31 +1,44 @@
 const express = require('express')
 const app = express()
-const items = require('./data.js')
-const bodyParser = require('body-parser')
+// const items = require('./data.js')
 
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json())
+const commentsRoute = require('./routes/Comments')
+app.use('/comments', commentsRoute)
 
-app.get('/items', (req, res) =>{
-    res.json(items)
-})
 
-app.post("/items", (req, res) => {
-  const newItem = {
-    name: req.body.name,
-    id: req.body.id,
-    price: req.body.price,
-  }
-  items.push(newItem)
-  res.json(items)
-})
+const postsRouter = require('./routes/Posts')
+app.use('/posts', postsRouter)
 
-app.delete("/items:name", (req, res) =>{
+const storiesRouter = require('./routes/Stories')
+app.use('/stories', storiesRouter)
 
-})
+const likesRouter = require('./routes/Likes')
+app.use('/likes', likesRouter)
+
+// const bodyParser = require('body-parser')
+
+// app.use(bodyParser.urlencoded({extended: false}))
+// app.use(bodyParser.json())
+
+// app.get('/items', (req, res) =>{
+//     res.json(items)
+// })
+
+// app.post("/items", (req, res) => {
+//   const newItem = {
+//     name: req.body.name,
+//     id: req.body.id,
+//     price: req.body.price,
+//   }
+//   items.push(newItem)
+//   res.json(items)
+// })
+
+// app.delete("/items:name", (req, res) =>{
+
+// })
 
 
 app.listen(4000, () =>{
     console.log('My port is open at 4000...')
-}
-)
+})
